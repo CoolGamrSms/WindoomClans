918d561455a4c917223943bc62c2416f1157fd05
CoolGamrSms <schulte.shane@gmail.com>
2016-04-27 04:12:36 -0400

    started structuring

---
+++ src/main/java/com/shaneschulte/windoom/clans/WindoomClans.java
@@ -0,0 +1,21 @@
+package com.shaneschulte.windoom.clans;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Shane Schulte (CoolGamrSms on 4/27/2016.
+ */
+public class WindoomClans extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
+
